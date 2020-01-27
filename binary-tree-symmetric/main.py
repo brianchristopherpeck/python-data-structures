3b5b5c26d43ecef079f7e65937287bebd8357d15
Brian Peck <brian.peck@thecontrolgroup.com>
2020-01-26 22:06:30 -0800

    Build tree and fix comment

--- binary-tree-symmetric/main.py
+++ binary-tree-symmetric/main.py
@@ -1,4 +1,4 @@
-Definition for a binary tree node.
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -16,3 +16,28 @@ class Solution:
             return False
         
         return (t1.val == t2.val) and self.isMirror(t1.right,t2.left) and self.isMirror(t1.left,t2.right)
+
+# Tree Node
+#          3
+#        /   \
+#       9     9
+#      / \    / \
+#     7   6  6   7
+#        /    \  
+#       11     11 
+ 
+
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.left.left = TreeNode(7)
+root.left.right = TreeNode(6)
+root.left.right.left = TreeNode(11)
+
+root.right = TreeNode(9)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+root.right.left.right = TreeNode(11)
+
+result = Solution().isSymmetric(root)
+print(result)
