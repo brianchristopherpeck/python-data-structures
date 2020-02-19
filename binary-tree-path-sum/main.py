12c90dc7f7915b1daa5daa5c83befc1eb9e02a38
Brian Peck <brian.peck@thecontrolgroup.com>
2020-02-18 23:45:29 -0800

    binary tree path sum

---
+++ binary-tree-path-sum/main.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        
+        de = [(root, sum - root.val),]
+        while de:
+            node, curr_sum = de.pop()
+            if not node.left and not node.right and curr_sum == 0:
+                return True
+            if node.right:
+                de.append((node.right, curr_sum - node.right.val))
+            if node.left:
+                de.append((node.left, curr_sum - node.left.val))
+        return False
+
+#       5
+#      / \
+#     4   8
+#    /   / \
+#   11  13  4
+#  /  \      \
+# 7    2      1
+
+root = TreeNode(5)
+root.left = TreeNode(4)
+root.left.left = TreeNode(11)
+root.left.left.left = TreeNode(7)
+root.left.left.right = TreeNode(2)
+
+root.right = TreeNode(8)
+root.right.left = TreeNode(13)
+root.right.right = TreeNode(4)
+root.right.right.right = TreeNode(1)
+
+result = Solution().hasPathSum(root, 22)
+print(result)
+
+# should be True
